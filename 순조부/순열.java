2289ae1ee8c90f069f73b0d41e6a663e53c71638
JHYUN <37072549+jhyun3315@users.noreply.github.com>
2023-07-07 20:40:03 +0900

    Update 순열.java

--- 순조부/순열.java
+++ 순조부/순열.java
@@ -13,28 +13,26 @@ public class 순열 {
         int[] arr = {1, 3, 5};
         // 담을배열
         int[] sel = new int[2];
-        // 뽑음여부 판단 배열
-        boolean[] visited = new boolean[3];
+        
+        recursive(arr, sel, new boolean[arr.length],0);
+	}
 
-        permutation(arr, sel, visited ,0);
+	private static void recursive(int[] arr, int[] sel, boolean[] visited, int idx) {
+		if (idx == sel.length) {
+			System.out.println(Arrays.toString(sel));
+			return;
+		}
 
-    }
+		for (int i = 0; i < arr.length; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				sel[idx] = arr[i];
+				recursive(arr, sel, visited, idx + 1);
+				visited[i] = false;
+			}
+		}
+	}
 
-    public static void permutation(int[] arr, int[] sel, boolean[] visited, int dept) {
-        if (dept == sel.length) {
-            System.out.println(Arrays.toString(sel));
-            return;
-        }
 
-        for (int i = 0; i < arr.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                sel[dept] = arr[i];
-                permutation(arr, sel, visited, dept + 1);
-                visited[i] = false;
-            }
-        }
-
-    }
 }
 
