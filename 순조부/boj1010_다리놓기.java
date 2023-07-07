21b3f7f64bb5d8ca5c424ad5104be0f0400e54f4
JHYUN <37072549+jhyun3315@users.noreply.github.com>
2023-07-07 21:35:55 +0900

    Create boj1010_다리놓기.java

---
+++ 순조부/boj1010_다리놓기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+/*
+    백준 - 다리놓기
+    https://www.acmicpc.net/problem/1010
+ */
+
+public class boj1010_다리놓기 {
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T =  Integer.parseInt(br.readLine());
+        dp = new int[30][30];
+
+        for(int i=0;i<T;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int R = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
+            System.out.println(Combination(N, R));
+        }
+    }
+    public static int Combination(int n, int r){
+        if(dp[n][r] > 0) return dp[n][r];
+        if(n==r || r==0) return dp[n][r] =1;
+        return dp[n][r] = Combination(n-1,r-1) + Combination(n-1, r);
+    }
+
+}
